008a6f9983a1f3c061369afeca2212c8a5c2c3eb
KissCode <417758082@qq.com>
2015-04-24 15:21:55 +0800

    tool

---
+++ MvcOA.IDAL/IBaseDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcOA.IDAL
+{
+    public interface IBaseDAL<T> where T : class,new()
+    {
+        T Add(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+        bool Delete(params int[] ids);
+        IQueryable<T> GetEntities(Func<T, bool> whereLamada);
+        IQueryable<T> GetPagesEntities<S>(int pageIndex, int pageSize, out int totalPage, Func<T, bool> whereLamada, Func<S, T> orderByLamada, bool isAsc);
+    }
+}
